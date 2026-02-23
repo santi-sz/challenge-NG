c1ac93341200be2c2e58661ec23191e7d0c0605e
santi-sz <santiago.sanchez9@mi.unc.edu.ar>
2026-02-23 11:05:53 -0300

    chore: inicializado el proyecto React con Vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Challenge NG</h1>
+        <p>Job Application Portal</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
